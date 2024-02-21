70b929b48d5f9b816719ac4a8b06c8bf904d81a2
sami <sami@samis-mac-mini.home>
2024-02-21 02:22:31 +0100

    feat : step 2 stack of dense layers and activation functions

---
+++ step2.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow as tf
+
+
+model = keras.Sequential([
+    layers.Dense(32, input_shape=[8]),
+    layers.Activation('relu'),
+    layers.Dense(32),
+    layers.Activation('relu'),
+    layers.Dense(1),
+])
+activation_layer = layers.Activation('swish')
+
+x = tf.linspace(-3.0, 3.0, 100)
+y = activation_layer(x) # once created, a layer is callable just like a function
+
+plt.figure(dpi=100)
+plt.plot(x, y)
+plt.xlim(-3, 3)
+plt.xlabel("Input")
+plt.ylabel("Output")
+plt.show()
