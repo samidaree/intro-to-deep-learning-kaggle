4210e96e7ca6174926994626ca4721858110ea56
sami <sami@samis-mac-mini.home>
2024-02-24 17:44:49 +0100

    feat : step 6 binary classification

--- step5.py
+++ step5.py
@@ -1,11 +1,8 @@
 from tensorflow.keras.callbacks import EarlyStopping
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.model_selection import GroupShuffleSplit
+
+
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.model_selection import GroupShuffleSplit
+
 
 from tensorflow import keras
 from tensorflow.keras import layers
---
+++ step6.py
@@ -0,0 +1,162 @@
+from tensorflow.keras.callbacks import EarlyStopping
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+import matplotlib.pyplot as plt
+
+
+import pandas as pd
+from IPython.display import display
+
+""" ion = pd.read_csv('./input/ion.csv')
+
+display(ion.head())
+
+df = ion.copy()
+df['Class'] = df['Class'].map({'good': 0, 'bad': 1})
+
+df_train = df.sample(frac=0.7, random_state=0)
+df_valid = df.drop(df_train.index)
+
+max_ = df_train.max(axis=0)
+min_ = df_train.min(axis=0)
+
+df_train = (df_train - min_) / (max_ - min_)
+df_valid = (df_valid - min_) / (max_ - min_)
+df_train.dropna(axis=1, inplace=True) # drop the empty feature in column 2
+df_valid.dropna(axis=1, inplace=True)
+
+X_train = df_train.drop('Class', axis=1)
+X_valid = df_valid.drop('Class', axis=1)
+y_train = df_train['Class']
+y_valid = df_valid['Class']
+
+model = keras.Sequential([
+    layers.Dense(4, activation='relu', input_shape=[34]),
+    layers.Dense(4, activation='relu'),    
+    layers.Dense(1, activation='sigmoid'),
+])
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['binary_accuracy'],
+)
+
+early_stopping = keras.callbacks.EarlyStopping(
+    patience=10,
+    min_delta=0.001,
+    restore_best_weights=True,
+)
+
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=1000,
+    callbacks=[early_stopping],
+    verbose=0, # hide the output because we have so many epochs
+)
+
+
+history_df = pd.DataFrame(history.history)
+# Start the plot at epoch 5
+history_df.loc[5:, ['loss', 'val_loss']].plot()
+history_df.loc[5:, ['binary_accuracy', 'val_binary_accuracy']].plot()
+
+print(("Best Validation Loss: {:0.4f}" +\
+      "\nBest Validation Accuracy: {:0.4f}")\
+      .format(history_df['val_loss'].min(), 
+              history_df['val_binary_accuracy'].max()))
+
+plt.show(block = 'true') """
+
+hotel = pd.read_csv('./input/hotel.csv')
+
+X = hotel.copy()
+y = X.pop('is_canceled')
+
+X['arrival_date_month'] = \
+    X['arrival_date_month'].map(
+        {'January':1, 'February': 2, 'March':3,
+         'April':4, 'May':5, 'June':6, 'July':7,
+         'August':8, 'September':9, 'October':10,
+         'November':11, 'December':12}
+    )
+
+features_num = [
+    "lead_time", "arrival_date_week_number",
+    "arrival_date_day_of_month", "stays_in_weekend_nights",
+    "stays_in_week_nights", "adults", "children", "babies",
+    "is_repeated_guest", "previous_cancellations",
+    "previous_bookings_not_canceled", "required_car_parking_spaces",
+    "total_of_special_requests", "adr",
+]
+features_cat = [
+    "hotel", "arrival_date_month", "meal",
+    "market_segment", "distribution_channel",
+    "reserved_room_type", "deposit_type", "customer_type",
+]
+
+transformer_num = make_pipeline(
+    SimpleImputer(strategy="constant"), # there are a few missing values
+    StandardScaler(),
+)
+transformer_cat = make_pipeline(
+    SimpleImputer(strategy="constant", fill_value="NA"),
+    OneHotEncoder(handle_unknown='ignore'),
+)
+
+preprocessor = make_column_transformer(
+    (transformer_num, features_num),
+    (transformer_cat, features_cat),
+)
+
+# stratify - make sure classes are evenlly represented across splits
+X_train, X_valid, y_train, y_valid = \
+    train_test_split(X, y, stratify=y, train_size=0.75)
+
+X_train = preprocessor.fit_transform(X_train)
+X_valid = preprocessor.transform(X_valid)
+
+input_shape = [X_train.shape[1]]
+
+model = keras.Sequential([
+    layers.BatchNormalization(input_shape = input_shape), 
+    layers.Dense(256, activation ='relu'), 
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(units = 256, activation = 'relu'), 
+    layers.BatchNormalization(), 
+    layers.Dropout(0.3), 
+    layers.Dense(1,activation = 'sigmoid')
+])
+
+model.compile(
+    optimizer= 'adam', 
+    loss = 'binary_crossentropy',
+    metrics = ['binary_accuracy']
+) 
+early_stopping = keras.callbacks.EarlyStopping(
+    patience=5,
+    min_delta=0.001,
+    restore_best_weights=True,
+)
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=200,
+    callbacks=[early_stopping],
+)
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot(title="Cross-entropy")
+history_df.loc[:, ['binary_accuracy', 'val_binary_accuracy']].plot(title="Accuracy")
+plt.show(block=True)
